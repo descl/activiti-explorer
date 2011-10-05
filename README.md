37cb538ebf0a827c448bcdea4fb4f4473af3e429
Gary Fredericks <fredericksgary@gmail.com>
2011-10-05 13:29:10 -0400

    Reimplemented LoginHandler in clojure

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ is a subset of the activiti svn repository, with history starting at release
 ## Differences
 
 - Shibboleth Login
+
+## Building
+
+I don't know maven, so I don't know what I'm doing,
+ but right now I can get it to compile with
+`mvn clean compile clojure:compile package`.
---
+++ src/main/clojure/hssc/activiti/identity/login_handler.clj
@@ -0,0 +1,31 @@
+(ns hssc.activiti.identity.login-handler
+  (:import javax.servlet.http.HttpServletRequest)
+  (:gen-class
+    :name hssc.activiti.identity.LoginHandler
+    :implements [org.activiti.explorer.ui.login.LoginHandler]))
+
+
+
+(defn -onRequestStart [& args])
+(defn -onRequestEnd [& args])
+(defn -logout [& args])
+
+(defrecord ShibUser [uid first-name last-name full-name]
+  org.activiti.explorer.identity.LoggedInUser
+  (getId [_] uid)
+  (getFirstName [_] first-name)
+  (getLastName  [_] last-name)
+  (getFullName  [_] full-name)
+  (getPassword  [_])
+  (isAdmin      [_] false)
+  (isUser       [_] true))
+
+(defn -authenticate
+  [_ req resp]
+  ; The interface authenticate method is overloaded, and if the args
+  ; are a different type we just return nil
+  (when (instance? HttpServletRequest req)
+    (when-let [uid (.getHeader req "uid")]
+      (let [[first-name last-name full-name]
+              (for [s ["givenName" "sn" "cn"]] (.getHeader req s))]
+        (new ShibUser uid first-name last-name full-name)))))
