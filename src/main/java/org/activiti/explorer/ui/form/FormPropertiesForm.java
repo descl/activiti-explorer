1e12d46d20a168e20d819a4b37c23f44c82a6138
Gary Fredericks <fredericksgary@gmail.com>
2011-11-25 22:54:23 -0500

    Implemented custom/multiple submit buttons feature

--- src/main/java/org/activiti/explorer/ui/form/FormPropertiesForm.java
+++ src/main/java/org/activiti/explorer/ui/form/FormPropertiesForm.java
@@ -13,8 +13,10 @@
 
 package org.activiti.explorer.ui.form;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.activiti.engine.FormService;
 import org.activiti.engine.ProcessEngines;
@@ -26,14 +28,14 @@ import org.activiti.explorer.ui.mainlayout.ExplorerLayout;
 import com.vaadin.data.Validator.InvalidValueException;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Component.Event;
 import com.vaadin.ui.VerticalLayout;
-
 
 /**
  * Form that renders form-properties and allows posting the filled in value. Performs
@@ -43,156 +45,214 @@ import com.vaadin.ui.VerticalLayout;
  * @author Frederik Heremans
  */
 public class FormPropertiesForm extends VerticalLayout {
-  
+
   private static final long serialVersionUID = -3197331726904715949L;
 
-  // Services
-  protected FormService formService;
-  protected I18nManager i18nManager;
+	// Services
+	protected FormService formService;
+	protected I18nManager i18nManager;
 
-  // UI
-  protected Label formTitle;
-  protected Button submitFormButton;
-  protected Button cancelFormButton;
-  protected FormPropertiesComponent formPropertiesComponent;
-  
-  public FormPropertiesForm() {
-    super();
-    formService = ProcessEngines.getDefaultProcessEngine().getFormService();
-    i18nManager = ExplorerApp.get().getI18nManager();
-    
-    addStyleName(ExplorerLayout.STYLE_DETAIL_BLOCK);
-    addStyleName(ExplorerLayout.STYLE_FORM_PROPERTIES);
-    
-    initTitle();
-    initFormPropertiesComponent();
-    initButtons();
-    initListeners();
-  }
-  
-  public void setFormProperties(List<FormProperty> formProperties) {
-    // Component will refresh it's components based on the passed properties
-    formPropertiesComponent.setFormProperties(formProperties);
-  }
-  
-  public void setSubmitButtonCaption(String caption) {
-    submitFormButton.setCaption(caption);
-  }
-  
-  public void setCancelButtonCaption(String caption) {
-    cancelFormButton.setCaption(caption);
-  }
-  
-  public void setFormHelp(String caption) {
-    formTitle.setValue(caption);
-    formTitle.setVisible(caption != null);
-  }
-  
-  /**
-   * Clear all (writable) values in the form.
-   */
-  public void clear() {
-    formPropertiesComponent.setFormProperties(formPropertiesComponent.getFormProperties());
-  }
+	// UI
+	protected Label formTitle;
+	protected List<ButtonWithValue> submitFormButtons;
+	protected FormPropertiesComponent formPropertiesComponent;
+	private HorizontalLayout buttonHolder;
+	private FormProperty submitProperty;
+	private Button submitButton;
+	private Button cancelButton;
 
-  protected void initTitle() {
-    formTitle = new Label();
-    formTitle.addStyleName(ExplorerLayout.STYLE_H4);
-    formTitle.setVisible(false);
-    addComponent(formTitle);
-  }
- 
-  protected void initButtons() {
-    submitFormButton = new Button();
-    cancelFormButton = new Button();
-    
-    HorizontalLayout buttons = new HorizontalLayout();
-    buttons.setSpacing(true);
-    buttons.setWidth(100, UNITS_PERCENTAGE);
-    buttons.addStyleName(ExplorerLayout.STYLE_DETAIL_BLOCK);
-    buttons.addComponent(submitFormButton);
-    buttons.setComponentAlignment(submitFormButton, Alignment.BOTTOM_RIGHT);
-    
-    buttons.addComponent(cancelFormButton);
-    buttons.setComponentAlignment(cancelFormButton, Alignment.BOTTOM_RIGHT);
-    
-    Label buttonSpacer = new Label();
-    buttons.addComponent(buttonSpacer);
-    buttons.setExpandRatio(buttonSpacer, 1.0f);
-    addComponent(buttons);
-  }
+	public FormPropertiesForm() {
+		super();
+		formService = ProcessEngines.getDefaultProcessEngine().getFormService();
+		i18nManager = ExplorerApp.get().getI18nManager();
 
-  protected void initFormPropertiesComponent() {
-    formPropertiesComponent = new FormPropertiesComponent();
-    addComponent(formPropertiesComponent);    
-  }
-  
-  protected void initListeners() {
-    submitFormButton.addListener(new ClickListener() {
-      
-      private static final long serialVersionUID = -6091586145870618870L;
-    
-      public void buttonClick(ClickEvent event) {
-        // Extract the submitted values from the form. Throws exception when validation fails.
-        try {
-          Map<String, String> formProperties = formPropertiesComponent.getFormPropertyValues();
-          fireEvent(new FormPropertiesEvent(FormPropertiesForm.this, FormPropertiesEvent.TYPE_SUBMIT, formProperties));
-          submitFormButton.setComponentError(null);
-        } catch(InvalidValueException ive) {
-          // Error is presented to user by the form component
-        }
-      }
-    });
-    
-    cancelFormButton.addListener(new ClickListener() {
-      
-      private static final long serialVersionUID = -8980500491522472381L;
+		addStyleName(ExplorerLayout.STYLE_DETAIL_BLOCK);
+		addStyleName(ExplorerLayout.STYLE_FORM_PROPERTIES);
 
-      public void buttonClick(ClickEvent event) {
-        fireEvent(new FormPropertiesEvent(FormPropertiesForm.this, FormPropertiesEvent.TYPE_CANCEL));
-        submitFormButton.setComponentError(null);
-      }
-    });
-  }
-  
-  protected void addEmptySpace(ComponentContainer container) {
-    Label emptySpace = new Label("&nbsp;", Label.CONTENT_XHTML);
-    emptySpace.setSizeUndefined();
-    container.addComponent(emptySpace);
-  }
-  
-  /**
-   * Event indicating a form has been submitted or cancelled. When submitted,
-   * the values of the form-properties are available.
-   * 
-   * @author Frederik Heremans
-   */
-  public class FormPropertiesEvent extends Event {
+		initTitle();
+		initFormPropertiesComponent();
+		initButtons();
+		initListeners();
+	}
 
-    private static final long serialVersionUID = -410814526942034125L;
-    
-    public static final String TYPE_SUBMIT = "SUBMIT";
-    public static final String TYPE_CANCEL = "CANCEL";
-    
-    private String type;
-    private Map<String, String> formProperties;
-    
-    public FormPropertiesEvent(Component source, String type) {
-      super(source);
-      this.type = type;
-    }
-    
-    public FormPropertiesEvent(Component source, String type, Map<String, String> formProperties) {
-      this(source, type);
-      this.formProperties = formProperties;
-    }
-    
-    public String getType() {
-      return type;
-    }
-    
-    public Map<String, String> getFormProperties() {
-      return formProperties;
-    }
-  }
+	public void setFormHelp(String caption) {
+		formTitle.setValue(caption);
+		formTitle.setVisible(caption != null);
+	}
+
+	/**
+	 * Clear all (writable) values in the form.
+	 */
+	public void clear() {
+		formPropertiesComponent.setFormProperties(formPropertiesComponent
+				.getFormProperties());
+	}
+	
+	public void setSubmitButtonCaption(String cap){
+		if(submitButton!=null)
+			submitButton.setCaption(cap);
+	}
+	
+	public void setCancelButtonCaption(String cap){
+		if(cancelButton!=null)
+			cancelButton.setCaption(cap);
+	}
+
+	protected void initTitle() {
+		formTitle = new Label();
+		formTitle.addStyleName(ExplorerLayout.STYLE_H4);
+		formTitle.setVisible(false);
+		addComponent(formTitle);
+	}
+
+	protected void initButtons() {
+		submitFormButtons = new LinkedList<ButtonWithValue>();
+		buttonHolder = new HorizontalLayout();
+		buttonHolder.setSpacing(true);
+		buttonHolder.setWidth(100, UNITS_PERCENTAGE);
+		buttonHolder.addStyleName(ExplorerLayout.STYLE_DETAIL_BLOCK);
+		addButtons();
+		addComponent(buttonHolder);
+	}
+
+	protected Button addButton(String id, String name) {
+		Button b = new Button();
+		b.setCaption(name);
+		buttonHolder.addComponent(b);
+		buttonHolder.setComponentAlignment(b, Alignment.BOTTOM_RIGHT);
+		submitFormButtons.add(new ButtonWithValue(b, id));
+		return b;
+	}
+
+	protected void addButtons() {
+		if (submitProperty == null) {
+			submitButton = addButton(null, "Complete");
+			cancelButton = addButton(null, "Cancel");
+		} else {
+			// I believe this is a map from ids to names
+			Map<String, String> formValues = (Map<String, String>) submitProperty
+					.getType().getInformation("values");
+			for (Entry<String, String> formValue : formValues.entrySet()) {
+				addButton(formValue.getKey(), formValue.getValue());
+			}
+		}
+
+		Label buttonSpacer = new Label();
+		buttonHolder.addComponent(buttonSpacer);
+		buttonHolder.setExpandRatio(buttonSpacer, 1.0f);
+
+	}
+
+	protected void initFormPropertiesComponent() {
+		formPropertiesComponent = new FormPropertiesComponent();
+		addComponent(formPropertiesComponent);
+	}
+
+	private class ClickListenerWithValue implements ClickListener {
+		private final String value;
+		private final Button button;
+
+		public ClickListenerWithValue(ButtonWithValue bwv) {
+			super();
+			this.value = bwv.value;
+			this.button = bwv.button;
+		}
+
+		public void buttonClick(ClickEvent event) {
+			// Extract the submitted values from the form. Throws
+			// exception when validation fails.
+			try {
+				Map<String, String> formProperties = formPropertiesComponent
+						.getFormPropertyValues();
+				if (value != null) {
+					formProperties.put(submitProperty.getId(), value);
+				}
+				fireEvent(new FormPropertiesEvent(
+						FormPropertiesForm.this,
+						FormPropertiesEvent.TYPE_SUBMIT, formProperties));
+				this.button.setComponentError(null);
+			} catch (InvalidValueException ive) {
+				// Error is presented to user by the form component
+			}
+		}
+	}
+
+	protected void initListeners() {
+		for (ButtonWithValue bwv : submitFormButtons) {
+			bwv.button.addListener(new ClickListenerWithValue(bwv));
+		}
+	}
+
+	protected void addEmptySpace(ComponentContainer container) {
+		Label emptySpace = new Label("&nbsp;", Label.CONTENT_XHTML);
+		emptySpace.setSizeUndefined();
+		container.addComponent(emptySpace);
+	}
+
+	private class ButtonWithValue {
+		public final Button button;
+		public final String value;
+
+		public ButtonWithValue(Button b, String v) {
+			button = b;
+			value = v;
+		}
+	}
+
+	/**
+	 * Event indicating a form has been submitted or cancelled. When submitted,
+	 * the values of the form-properties are available.
+	 * 
+	 * @author Frederik Heremans
+	 */
+	public class FormPropertiesEvent extends Event {
+
+		private static final long serialVersionUID = -410814526942034125L;
+
+		public static final String TYPE_SUBMIT = "SUBMIT";
+		public static final String TYPE_CANCEL = "CANCEL";
+
+		private String type;
+		private Map<String, String> formProperties;
+
+		public FormPropertiesEvent(Component source, String type) {
+			super(source);
+			this.type = type;
+		}
+
+		public FormPropertiesEvent(Component source, String type,
+				Map<String, String> formProperties) {
+			this(source, type);
+			this.formProperties = formProperties;
+		}
+
+		public String getType() {
+			return type;
+		}
+
+		public Map<String, String> getFormProperties() {
+			return formProperties;
+		}
+	}
+
+	public void setFormProperties(List<FormProperty> formProperties) {
+		submitProperty = null;
+		List<FormProperty> newList = new LinkedList<FormProperty>();
+		for (FormProperty fp : formProperties) {
+			if (fp.getName().equals("__SUBMIT__")
+					&& fp.getType().getName().equals("enum"))
+				submitProperty = fp;
+			else
+				newList.add(fp);
+		}
+		if (submitProperty != null)
+			resetButtons(submitProperty);
+		formPropertiesComponent.setFormProperties(newList);
+	}
+
+	protected void resetButtons(FormProperty submitProperty) {
+		buttonHolder.removeAllComponents();
+		addButtons();
+		initListeners();
+	}
 }
